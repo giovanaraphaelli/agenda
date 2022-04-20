9d0ac06545d27f6dc226f0b88646e8c26f4e49fb
Auto Logon <autologon@CSED.LAB>
2022-04-19 21:32:37 -0300

    finalizando a aula

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.get('/contatos', (req, res)=>{
     });
 });
 
+app.post('/contatos', (req, res)=>{
+    let dados = req.body;
+    let cmd_insert = 'INSERT INTO CONTATO SET ?';
+    db.query(cmd_insert,dados,(error, result)=>{
+        if(error){
+            res.status(400).json({message:"Erro: " + error});
+        }else{
+            res.status(201).json({message: result.insertID + " - Contato salvo!"});
+        }
+    });
+});
+
 app.listen(port, ()=>{
     console.log("Projeto Executando na porta: " + port);
 })
